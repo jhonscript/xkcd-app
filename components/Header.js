3ed349900c9c782b1d12cfe3823569dad8381d96
JhonScript <jguevarave@gmail.com>
2022-11-24 12:33:51 -0500

    Creation of navigation bar and comic listing

---
+++ components/Header.js
@@ -0,0 +1,22 @@
+import { Navbar, Text } from "@nextui-org/react";
+
+export function Header() {
+  return (
+    <header>
+      <Navbar isBordered>
+        <Navbar.Brand>
+          <Text b color="inherit" hideIn="xs">
+            XKCD
+          </Text>
+        </Navbar.Brand>
+        <Navbar.Content hideIn="xs">
+          <Navbar.Link isActive href="/">
+            Home
+          </Navbar.Link>
+          <Navbar.Link href="/about">About</Navbar.Link>
+          <Navbar.Link href="/search">Search</Navbar.Link>
+        </Navbar.Content>
+      </Navbar>
+    </header>
+  );
+}
